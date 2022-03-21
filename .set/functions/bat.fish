89a7b4f69fc610610f3f716b50ca9879f99c10d6
ivam3 <ivam3.bh@gmail.com>
2022-03-21 14:16:32 -0600

    function bat added

---
+++ .set/functions/bat.fish
@@ -0,0 +1,3 @@
+function bat
+   bat --style header,rule,snip,changes,header --theme 'Visual Studio Dark+' $argv
+end
--- .set/functions/cat.fish
+++
@@ -1,3 +0,0 @@
-function cat
-   bat --style header --style rules --style snip --style changes --style header $argv
-end
