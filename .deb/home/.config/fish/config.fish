3d8e9b655ec7711e3ffa984e00185f5d195db2a6
ivam3 <ivam3.bh@gmail.com>
2022-05-21 18:00:10 -0500

    update at v3

--- .deb/home/.config/fish/config.fish
+++ .deb/home/.config/fish/config.fish
@@ -5,18 +5,4 @@ if status is-interactive
 	#  [else if CONDITION2; COMMANDS_TRUE2 ...;]
 	#  [else; COMMANDS_FALSE ...;]
 	#  end
-
-  if test -f /data/data/com.termux/files/usr/bin/fish
-	if ! grep -oE "i-Haklab" /data/data/com.termux/files/usr/etc/fish/config.fish >/dev/null
-		and ! -h /data/data/com.termux/files/usr/etc/fish/config.fish
-			echo -en "\e[33mWARNING:\e[0m Setting shell ..."
-			rm /data/data/com.termux/files/usr/etc/fish/config.fish
-			ln -s /data/data/com.termux/files/home/.local/etc/fish/config.fish /data/data/com.termux/files/usr/etc/fish/config.fish
-	end
-	chsh -s fish
-  else
-	echo -en "\e[33mWARNING:\e[0m Shell missing, installing ..."
-	yes|apt install fish &>/dev/null 2>/dev/null
-	exit
-  end
 end
