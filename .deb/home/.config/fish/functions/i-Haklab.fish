f52a68c9a695db303d36c23a1b9a718b117fd09a
ivam3 <ivam3.bh@gmail.com>
2022-05-22 05:07:08 -0500

    update i-haklab at v3.5-stable

--- .deb/home/.config/fish/functions/i-Haklab.fish
+++ .deb/home/.config/fish/functions/i-Haklab.fish
@@ -4,7 +4,7 @@ function i-Haklab
 	else if test $argv[1] = "password"
 		set argv passwd pass
 	else if ! ls /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] &>/dev/null
-		echo -en "\e[31m(_➤)\e[0m missing argument, type i-Haklab help for helpper"
+		echo -en "\e[31m(➤_)\e[0m missing argument, type i-Haklab help for helpper"
 	end
 	bash /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] $argv[2..-1]
 end
