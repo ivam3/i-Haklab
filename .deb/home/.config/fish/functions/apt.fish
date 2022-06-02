5b3d7ef3ba917fd0576ccb3f166bd805e3e129e1
ivam3 <ivam3.bh@gmail.com>
2022-06-02 05:33:01 -0500

    update at v3.5.1

--- .deb/home/.config/fish/functions/apt.fish
+++ .deb/home/.config/fish/functions/apt.fish
@@ -10,8 +10,18 @@ function apt
 					echo -en "\e[31mE:\e[0m $i is a ruby gem, you should try it with \e[33m'gem install $i'\e[0m\n"
 				
 				case localtunnel
-					echo -e  "\e[31mE:\e[0m $i is a node module, you should try it with \e[33m'npm install -g $i'\e[0m\n"
-				
+					echo -en "\e[31mE:\e[0m $i is a node module, you should try it with \e[33m'npm install -g $i'\e[0m\n"
+
+				case omf
+					if command -v fish >/dev/null
+						echo "Installing oh-my-fish over fish shell ..."
+						curl -fsSL "https://raw.githubusercontent.com/ivam3/i-Haklab/master/.deb/home/.local/etc/i-Haklab/get.oh-my.fish" -o $TMPDIR/omf.fish
+						fish $TMPDIR/omf.fish
+						rm $TMPDIR/omf.fish
+					else
+						echo -en "\e[31mE:\e[0m Fish needed, you should try it with \e[33m'apt install fish'\e[0m\n"
+					end
+
 				case '*'
 					$PREFIX/bin/apt "$argv[1]" $i
 					if grep $i /data/data/com.termux/files/home/.local/etc/i-Haklab/Tools/listofpkg2conf &>/dev/null
