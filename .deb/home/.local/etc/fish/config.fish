8a9d570d9590128324ee814d5e86333c22df7d84
ivam3 <ivam3.bh@gmail.com>
2022-11-07 07:32:15 -0600

    enable a custom banner option

--- .deb/home/.local/etc/fish/config.fish
+++ .deb/home/.local/etc/fish/config.fish
@@ -32,12 +32,12 @@ set fish_theme will
 set -g theme_nerd_fonts yes
 set DISPLAY :0
 set PULSE_SERVER 127.0.0.1
-set PATH /data/data/com.termux/files/usr/bin:/data/data/com.termux/files/home/.local/bin
+set PATH /data/data/com.termux/files/usr/bin:/data/data/com.termux/files/home/.local/bin:/data/data/com.termux/files/home/go/bin
 set HOME /data/data/com.termux/files/home
 set GOPATH /data/data/com.termux/files/home/go
 set GOROOT /data/data/com.termux/files/usr/lib/go
 set JAVA_HOME /data/data/com.termux/files/usr/opt/openjdk
-set LD_LIBRARY_PATH /data/data/com.termux/files/usr/lib:/data/data/com.termux/files/home/.local/lib
+set LD_LIBRARY_PATH /data/data/com.termux/files/usr/lib
 set TOOLS /data/data/com.termux/files/home/.local/share
 alias bat="bat -f --theme 'Visual Studio Dark+'"
 alias postgresql="pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql"
