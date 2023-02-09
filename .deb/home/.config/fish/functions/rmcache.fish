4cdd127d65576424f239d23daec5d3e7e0de6efd
ivam3 <ivam3.bh@gmail.com>
2023-02-08 19:11:17 -0600

    update command rmcache

--- .deb/home/.config/fish/functions/rmcache.fish
+++ .deb/home/.config/fish/functions/rmcache.fish
@@ -1,6 +1,6 @@
 function rmcache
-	if test "~/.cache"
-    rm -rf ~/.cache
-  end
+  rm -rf ~/.cache 2>/dev/null
+  rm -rf $TMPDIR/* 2>/dev/null
   dpkg -l|grep '^rc'|awk '{print $2}'|xargs dpkg --purge 2>/dev/null
+  echo "Termux cache was removed."
 end
