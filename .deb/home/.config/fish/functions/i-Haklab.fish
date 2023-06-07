9891cef174ad4b869b82af5286682af96c6b12c7
ivam3 <ivam3.bh@gmail.com>
2023-06-07 02:13:44 -0600

    update neovim

--- .deb/home/.config/fish/functions/i-Haklab.fish
+++ .deb/home/.config/fish/functions/i-Haklab.fish
@@ -1,7 +1,5 @@
 function i-Haklab
-	if test (count $argv) -eq 0 >/dev/null
-		set argv help
-	else if ls /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] &>/dev/null
+	if test (ls /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1]) &>/dev/null
 		bash /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] $argv[2..-1]
 	else
 		echo -en "\e[31m(➤_)\e[0m missing argument, type i-Haklab help for helpper\n"
