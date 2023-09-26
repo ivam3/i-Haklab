d9ade4903f9822a44f8d40670d8154dd113d1e48
ivam3 <ivam3.bh@gmail.com>
2023-09-26 04:30:29 -0600

    fix some bugs in pkg2conf

--- .deb/home/.config/fish/functions/i-Haklab.fish
+++ .deb/home/.config/fish/functions/i-Haklab.fish
@@ -1,5 +1,5 @@
 function i-Haklab
-	if test (ls /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1]) &>/dev/null
+	if test (ls /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] 2>/dev/null)
 		bash /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] $argv[2..-1]
 	else
 		echo -en "\e[31m(➤_)\e[0m missing argument, type i-Haklab help for helpper\n"
