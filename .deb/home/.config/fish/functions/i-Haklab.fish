35781707cadc4780d12f4b7a8a5c8cc45b3e629d
ivam3 <ivam3.bh@gmail.com>
2023-10-21 10:54:27 -0600

    update list of tools

--- .deb/home/.config/fish/functions/i-Haklab.fish
+++ .deb/home/.config/fish/functions/i-Haklab.fish
@@ -1,8 +1,12 @@
 function i-Haklab
-  if test (count $argv) -gt 0 -a (test -f /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] 2>/dev/null)
-		bash /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] $argv[2..-1]
-	else
+  if test (count $argv) -gt 0
+    if test -f /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1]
+		  bash /data/data/com.termux/files/home/.local/libexec/i-Haklab/$argv[1] $argv[2..-1]
+    else
+		  echo -en "\e[31m(➤_)\e[0m invalid argument, type i-Haklab help for helpper\n"
+    end
+  else
 		echo -en "\e[31m(➤_)\e[0m missing argument, type i-Haklab help for helpper\n"
-	end
+  end
 end
 
