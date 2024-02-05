4ab5839af5aaa66dce67a509bc1128d3ff51a098
VictorH28 <demonhunternauta@gmail.com>
2024-02-05 08:03:38 -0500

    add fincion command

--- include/admin/AdminHaklab.h
+++ include/admin/AdminHaklab.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <cstdlib>
 #include <iostream>
 #include <set>
+#include <filesystem>
+
 namespace admin {
 class AdminHaklab {
 public:
   std::set<std::string> matchingFile(const std::string &from);
+  // Verifica si  un comando   existe   
+  bool command(const char &cm){
+    if (std::filesystem::exists("/bin/" + std::to_string(cm))) {
+     return true;
+    } else if (std::filesystem::exists(std::string(getenv("PREFIX")) + "/bin/" + cm)) {
+     return true; 
+    } 
+     return false;
+  };
 };
 } // namespace admin
