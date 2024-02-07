f961dea6af9a390683d3f8e00c7901742ea5e4c6
VictorH28 <demonhunternauta@gmail.com>
2024-02-07 17:21:29 -0500

    apt_install add

--- include/below_zero.h
+++ include/below_zero.h
@@ -7,11 +7,13 @@
 //------------------------------------------
 #include "../include/command_line_argument_parser.h"
 #include "../include/network/NetworHaklab.h"
+#include "../include/admin/AdminHaklab.h"
 #include <boost/beast/http/status.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
 #include <csignal>
 #include <cstdlib>
+#include <curl/curl.h>
 #include <fmt/color.h> // Un  mundo sin colores es  feo  .....
 #include <fmt/core.h>
 #include <fstream>
@@ -33,6 +35,7 @@ namespace fs = boost::filesystem;
 #define CLEAR_SCREEN_ANSI "\e[1;1H\e[2J";
 //------------------------------------------
 //------------------------------------------
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::fstream;
@@ -41,6 +44,7 @@ using std::vector;
 //------------------------------------------
 //------------------------------------------
 
+// Enumeracion con alcanse
 enum class Color {
   Default,
   Black,
@@ -85,32 +89,50 @@ std::string setColor(Color color);
  *
  */
 void syntax_highlight(const std::string &code);
+
+/*
+ * Clase principal
+ */
 class Haklab {
 private: // ---> Variables privadas
-  const char *_shell;
+  std::string _shell;
   command_line_argument_parser parser;
   network::NetworHakaklab network;
+  admin::AdminHaklab admin;
   string IHETC{string(getenv("HOME")) + "/.local/etc/i-Haklab"};
   string LIBEX{string(getenv("HOME")) + "/.local/libexec/i-Haklab"};
 
 public:
   // contructor
-  Haklab() {}
-  /*
-   * Atrapa el control c
-   */
-  void ctrl_c() { signal(SIGINT, k_boom); }
+  Haklab(Shell sh)
+  {
+  switch (sh) {
+    case Shell::zsh:
+      _shell = "zsh";
+      break;  
+    case Shell::fish:
+      _shell = "fish";
+      break;
+  }
+  // Install shell 
+  if(!admin.command(_shell)){
+    cout << "Instalamdo  "  << _shell;
+    std::string command {"apt install " + _shell };
+    std::system(command.c_str());
+  };
+  }
   /*
    *
    */
-/*  Haklab &Shell(shell::Shell &sh) {
-    switch (sh) {
-    default:
-      break;
-    }
-    return *this;
-  }*/
+  string apt_install(const string *pkg){
+    
+    return " ";
+  }
   /*
+   * Atrapa el control c  
+   */
+  void ctrl_c() { signal(SIGINT, k_boom); }
+   /*
    * inisializar
    */
   int run(int argc, const char *argv[]);
@@ -137,6 +159,30 @@ public:
     show_cursor();
   }      // loading
 private: // ---> Funciones pribada
+  // Descargar  arcivos   
+  //
+  bool download_file(std::string url, std::string outputFilename) {
+    CURL *curl;
+    FILE *fp;
+    CURLcode result;
+    curl = curl_easy_init();
+    if (curl) {
+      fp = fopen(outputFilename.c_str(), "wb");
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+      result = curl_easy_perform(curl);
+      // Verifica si la descarga se realizó correctamente
+      if (result != CURLE_OK) {
+        std::cerr << "Error a descargar archivo " << endl;
+        return EXIT_FAILURE;
+      }
+      curl_easy_cleanup(curl);
+      fclose(fp);
+    }
+    return EXIT_SUCCESS;
+  };
+
   /*  Comprueba la existencia de la key
    *  - advierte en auto si no existe   (HAKLAB-APT-OFF )   para    quitar
    *  - descarga en auto matico ( HAKLAB-APT-AUTO >> .zshrc)
