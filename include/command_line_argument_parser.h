3fbaeabbd8c27b0bf7a3f7c12c891259ec66be85
VictorH28 <demonhunternauta@gmail.com>
2024-07-06 00:36:42 -0400

    Se separa el codigo en mas clases para un mejor entendimiento

--- include/command_line_argument_parser.h
+++ include/command_line_argument_parser.h
@@ -9,10 +9,6 @@ using namespace std;
 class arguments {
   constexpr static auto help_option       = "help";
   constexpr static auto about_option_name = "about";
-  constexpr static auto username_option = "username,u";
-  constexpr static auto verbose_option_name = "verbose";
-  constexpr static auto verbose_option = "verbose,v";
-  constexpr static auto files_option_name = "input-files";
 
   boost::program_options::variables_map variables;
   friend class command_line_argument_parser;
@@ -25,8 +21,6 @@ public:
 
   bool help() { return variables.count(help_option) > 0; }
 
-  bool verbose() { return variables.count(verbose_option_name) > 0; }
-
 
   string about() {
     return (variables.count(about_option_name) > 0)
@@ -34,11 +28,6 @@ public:
                : "";
   }
 
-  const vector<string> filenames() {
-    return (variables.count(files_option_name) > 0)
-               ? variables[files_option_name].as<vector<string>>()
-               : vector<string>();
-  }
 };
 
 class command_line_argument_parser {
@@ -57,7 +46,7 @@ public:
     boost::program_options::variables_map variables;
 
     boost::program_options::positional_options_description p;
-    p.add(arguments::files_option_name, -1);
+    // p.add(arguments::files_option_name, -1);
 
     boost::program_options::store(
         boost::program_options::command_line_parser(argc, argv)
