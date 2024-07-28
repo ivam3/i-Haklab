5c5e6d347a403bced10bb8d0ba8332c8c255e2df
ivam3 <ivam3.bh@gmail.com>
2024-07-28 06:46:30 -0600

    fix banner reloaded by error

--- .deb/home/.config/fish/functions/github.fish
+++ .deb/home/.config/fish/functions/github.fish
@@ -1,6 +1,6 @@
 function github
-   cat /storage/0095-4180/Android/data/com.termux/files/Ivam3/repositories/.token | termux-clipboard-set
-	 pushd /storage/0095-4180/Android/data/com.termux/files/Ivam3/repositories/$argv[1]
+   cat /storage/0095-4180/Android/media/com.termux/Ivam3/repositories/.token | termux-clipboard-set
+	 pushd /storage/0095-4180/Android/media/com.termux/Ivam3/repositories/$argv[1]
 	 git add --all
 	 git commit -m "$argv[2..-1]"
 	 git push
