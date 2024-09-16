d288d7e560300e570550decab60d23781a420f47
VictorH28 <demonhunternauta@gmail.com>
2024-09-16 09:13:07 -0400

    Creador de

--- include/below_zero/deb/cre-deb.hpp
+++ include/below_zero/deb/cre-deb.hpp
@@ -2,10 +2,39 @@
 #define BELOW_ZERO_DEB
 
 #include <boost/filesystem.hpp>
+#include <boost/filesystem/directory.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <boost/json.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <list>
+#include <string>
 
-class Deb{
-   public:
+namespace fs = boost::filesystem;
+
+class CreateDeb{
+   std::list<std::string> files_conf {"control","src","doc","config"};
+   std::list<std::string> control_conf {}; 
+  public:
+     CreateDeb(fs::path proyec);
+ //    ~CreateDeb();
 };
+
+CreateDeb::CreateDeb(fs::path proyec){
+  if(!fs::exists(proyec)){
+    std::cerr << "File no found :" + proyec.string() << std::endl; 
+    exit(1);
+  }
+  // Crear  directoriios  de   trabajo   
+  for(std::string files : files_conf){
+    fs::create_directory(proyec / files);
+  };
+ 
+  // control  config   
+
+  for(fs::directory_entry &dir_entry : fs::directory_iterator{proyec}){
+  
+  };
+}
 
 #endif // !BELOW_ZERO_DEB
