dbd05240bdc72d8110ded85451a2ac95af2920bd
ivam3 <ivam3.bh@gmail.com>
2024-10-30 07:18:10 -0600

    update features

--- .deb/home/.config/fish/functions/codec.fish
+++ .deb/home/.config/fish/functions/codec.fish
@@ -2,8 +2,8 @@ function codec
 	if test "$argv[1]" = "r"
     ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$argv[2]"
   else if test "$argv[1]" = "c"
-    ffmpeg -i "$argv[2]" -vf scale=2048:858 -preset slow -crf 18 "$argv[2]_2048x858.mp4"
+    ffmpeg -i "$argv[2]" -vf scale=1920:1080 -preset slow -crf 18 "$argv[2]_1920x1080.mp4"
   else
-    echo "USAGE: codec [r = verify video resolution]<|>[c = codec video at 2048x858]"
+    echo "USAGE: codec [r = verify video resolution]<|>[c = codec video at 1920x1080]"
 	end
 end
