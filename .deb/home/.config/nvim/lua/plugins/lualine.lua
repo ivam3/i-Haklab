c9ec4bab3d933074894e24b60e9ecf4078a6f549
ivam3 <ivam3.bh@gmail.com>
2025-05-24 23:07:24 -0600

    fix deprecated vim.lsp.get_active_clients

--- .deb/home/.config/nvim/lua/plugins/lualine.lua
+++ .deb/home/.config/nvim/lua/plugins/lualine.lua
@@ -181,7 +181,7 @@ ins_left {
   function()
     local msg = '🔎 Lsp'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
