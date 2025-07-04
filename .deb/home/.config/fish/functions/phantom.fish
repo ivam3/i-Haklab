7a1fbf31587bc9545e2cf2b279cfa597f9fdfefc
ivam3 <ivam3.bh@gmail.com>
2025-07-03 19:16:11 -0600

    upgrade

--- .deb/home/.config/fish/functions/phantom.fish
+++ .deb/home/.config/fish/functions/phantom.fish
@@ -1,11 +1,13 @@
 function phantom
-    set phantom_limit (/system/bin/dumpsys activity settings | grep max_phantom_processes | awk -F "=" '{print $NF}')
+    set phantom_limit (/system/bin/dumpsys activity settings |\
+        grep max_phantom_processes | awk -F "=" '{print $NF}')
     
     if test $phantom_limit = "32"
         adb shell pm grant com.termux android.permission.PACKAGE_USAGE_STATS
         adb shell pm grant com.termux android.permission.DUMP
         adb shell "/system/bin/device_config put activity_manager max_phantom_processes 2147483647"
-        sleep 6
+        set phantom_limit (/system/bin/dumpsys activity settings |\
+            grep max_phantom_processes | awk -F "=" '{print $NF}')
     end
     
     echo "The limit of phantom process is: $phantom_limit"
