2ab6e386fd2f963a0777ae5292adf16abd2d933b
ivam3 <ivam3.bh@gmail.com>
2025-08-07 03:37:57 -0600

    deprecated options was removed

--- .deb/home/.config/fish/functions/cinderella.fish
+++
@@ -1,8 +0,0 @@
-function cinderella
-	if test (ls /data/data/com.termux/files/home/.local/libexec/cinderella 2>/dev/null)
-		bash /data/data/com.termux/files/home/.local/libexec/cinderella $argv
-	else
-		echo -en "\e[31m(➤_)\e[0m missing argument, type i-Haklab help for helpper\n"
-	end
-end
-
