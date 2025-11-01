3f199a2cb70dc96c25c6c610b8db9387ae75fcb0
ivam3 <ivam3.bh@gmail.com>
2025-11-01 16:34:46 -0600

    add compile variables CC & CXX

--- .deb/home/.local/etc/fish/config.fish
+++ .deb/home/.local/etc/fish/config.fish
@@ -41,6 +41,8 @@ set LD_LIBRARY_PATH /data/data/com.termux/files/usr/lib
 set LLVM_BUILD /data/data/com.termux/files/usr/lib
 set LLVM_HOME /data/data/com.termux/files/usr/lib/cmake/llvm
 set CFLAGS \ -Wno-incompatible-function-pointer-types
+set CC clang
+set CXX clang++
 set ANDROID_ALLOW_UNDEFINED_SYMBOLS On
 set TOOLS /data/data/com.termux/files/home/.local/share
 alias du="du -hP"
