57887aa126d326e330daf49c652d3de61a3c9095
𝕯𝖊𝖒𝖔𝖓 𝕳𝖚𝖓𝖙𝖊𝖗 <117833298+VictorH028@users.noreply.github.com>
2026-08-04 19:02:12 +0500

    no tienes ninguna tecla para confirmar la sugerencia
    Added confirmation mapping for completion.

--- .deb/home/.config/nvim/lua/plugins/lsp-zero.lua
+++ .deb/home/.config/nvim/lua/plugins/lsp-zero.lua
@@ -103,12 +103,15 @@ cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
+    
+mapping = cmp.mapping.preset.insert({
+  ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+  ['<C-n>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+  ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+}),
+    
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
