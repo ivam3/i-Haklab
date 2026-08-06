d6dd8e9a50ced93868d1a9b0a2af179270c31bec
𝕯𝖊𝖒𝖔𝖓 𝕳𝖚𝖓𝖙𝖊𝖗 <117833298+VictorH028@users.noreply.github.com>
2026-08-06 17:38:25 +0500

    Add formatters for shell, bash, cpp, and java
    Added support for additional file types in formatting.

--- .deb/home/.config/nvim/lua/plugins/formatting.lua
+++ .deb/home/.config/nvim/lua/plugins/formatting.lua
@@ -1,13 +1,13 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
     python = { "isort", "black" },
-    -- You can customize some of the format options for the filetype (:help conform.format)
     rust = { "rustfmt", lsp_format = "fallback" },
-    -- Conform will run the first available formatter
+    sh = { "shfmt" },
+    bash = { "shfmt" },
+    cpp = { "clang-format" },
+    java = { "google-java-format" },
     javascript = { "prettierd", "prettier", stop_after_first = true },
-    -- 
     C = {"clang-format"},
   },
 })
